c3dc1d761896804e50975eede2fc070050306133
0736b <thanawat_bumpengpun@cmu.ac.th>
2022-04-23 17:17:01 +0700

    add: router ejs render product

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,6 +13,19 @@ app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+productRouter.route('/').get((req,res) => {
+    res.render('products', {
+        products:[
+            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199},
+            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199},
+            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199},
+            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199}
+        ]
+    })
+});
+
+app.use('/products', productRouter)
 
 app.get('/', (req, res) => {
     res.render('index', {username: '0736b', customer: ['customer1', 'customer2', 'customer3']});
