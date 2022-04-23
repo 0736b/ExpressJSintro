793c4f5e86fb3a51dacfbb9cc48b3457a760b3d2
0736b <thanawat_bumpengpun@cmu.ac.th>
2022-04-23 17:24:28 +0700

    add: file data json and usage

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const products = require('./data/products.json');
 const productRouter = express.Router();
 
 const app = express();
@@ -15,14 +16,9 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 productRouter.route('/').get((req,res) => {
-    res.render('products', {
-        products:[
-            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199},
-            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199},
-            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199},
-            {productTitle: 'Cyanide', productDescription: 'Acid toxic', productPrice: 199}
-        ]
-    })
+    res.render('products', 
+        products,
+    )
 });
 
 app.use('/products', productRouter)
