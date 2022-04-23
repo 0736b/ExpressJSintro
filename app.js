2730d0ca685f204fcc784b0999369e6267d8e9a1
0736b <thanawat_bumpengpun@cmu.ac.th>
2022-04-23 17:42:53 +0700

    reloc: router

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const products = require('./data/products.json');
-const productRouter = express.Router();
+
+const productsRouter = require('./src/router/productsRouter');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -15,13 +15,7 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-productRouter.route('/').get((req,res) => {
-    res.render('products', 
-        products,
-    )
-});
-
-app.use('/products', productRouter)
+app.use('/products', productsRouter)
 
 app.get('/', (req, res) => {
     res.render('index', {username: '0736b', customer: ['customer1', 'customer2', 'customer3']});
